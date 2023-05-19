b88fe7cd6b793535b2e0bae04ed02e3cb602a092
noaysk <nyasukagawa@myseneca.ca>
2023-05-20 00:19:30 +0900

    added the layout

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+   
+    top: {
+
+        yellowWall: "./assets/top.webp",
+        hello: "./assets/hello.webp",
+        bubble: "./assets/bubble.webp",
+        icon: "./assets/icon.webp",
+
+
+      
+  
+     
+    },
+  
+  };
+  
+  export default config;
