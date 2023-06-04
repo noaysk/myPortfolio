96354402da715f4a691d68fa0cc2b8700842411d
noaysk <nyasukagawa@myseneca.ca>
2023-06-05 02:40:19 +0900

    added icons

--- src/config.js
+++ src/config.js
@@ -7,11 +7,31 @@ const config = {
         bubble: "./assets/bubble.webp",
         icon: "./assets/icon.webp",
 
-
-      
-  
-     
     },
+    icon:{
+
+        GitHub: "./assets/GitHub.webp",
+        LinkedIn: "./assets/LinkedIn.webp",
+        GitHubIcon: "./assets/GitHubIcon.webp",
+        LinkedInIcon: "./assets/LinkedInIcon.webp",
+  
+        game: "./assets/game.webp",
+        video: "./assets/video.webp",
+        ae: "./assets/ae.webp",
+
+        manga: "./assets/manga.webp",
+        paint: "./assets/paint.webp",
+        guitar: "./assets/guitar.webp",
+    },
+    socials: {
+        Github: "https://github.com/noaysk",
+        LinkedIn: "https://www.linkedin.com/in/noa-yasukagawa-2270ba1b8/",
+
+        youtube: "https://www.youtube.com",
+        instagram: "https://www.instagram.com",
+        twitter: "https://www.twitter.com",
+        tiktok: "",
+      },
   
   };
   
