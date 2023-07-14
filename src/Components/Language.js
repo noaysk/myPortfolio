0245821dfb730fb545cb339ad958c7ed34515edf
noaysk <nyasukagawa@myseneca.ca>
2023-07-13 23:09:02 -0400

    added animations

--- src/Components/Language.js
+++ src/Components/Language.js
@@ -1,23 +1,34 @@
+import { motion } from "framer-motion";
+
 const Language = () => {
   return (
     <section>
+       
       <h2 className="text-4xl">Language</h2>
-      <ul>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        onViewportEnter={(entry) => {
+          document.querySelector('.progress1').classList.add('progressEng')
+          document.querySelector('.progress2').classList.add('progressJp')
+
+        }}
+        />
+      <ul className="xl:grid xl:grid-cols-2">
         <li className="py-3">
-          <span className="text-xl">English</span>
-          <progress
-            className="progress progress-warning w-56 h-6"
-            value="70"
-            max="100"
-          ></progress>
+          <span className="text-xl pb-3 xl:pb-5">English</span>
+          <div class="w-[300px] h-4 xl:w-[600px] xl:h-6 bg-bgColorW rounded-full">
+                <div class="progress1 w-0 h-full text-center text-xs text-white bg-bgColorS rounded-full">
+                </div>
+            </div>
         </li>
         <li className="py-3">
-          <span className="text-xl">Japanese</span>
-          <progress
-            className="progress progress-warning w-56 h-6"
-            value="100"
-            max="100"
-          ></progress>
+          <span className="text-xl pb-3 xl:pb-5">Japanese</span>
+          <div class="w-[300px] h-4 xl:w-[600px] xl:h-6 bg-bgColorW rounded-full">
+                <div class="progress2 w-0 h-full text-center text-xs text-white bg-bgColorS rounded-full">
+                </div>
+            </div>
         </li>
       </ul>
     </section>
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,114 +1,194 @@
-const Skills = () => {
-  
+import { motion } from "framer-motion";
 
+const Skills = () => {
   return (
     <section className="mt-5 bg-bgColor ">
       <h2 className="text-4xl">Skills</h2>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        onViewportEnter={(entry) => {
+          var skills90 = document.querySelectorAll('.radial-progress-90')
+          for( var a = 0; a<skills90.length; a++){
+            skills90[a].classList.add('meter-90')
+          }
+          var skills80 = document.querySelectorAll(".radial-progress-80")
+          for( var b = 0; b<skills80.length; b++){
+            skills80[b].classList.add('meter-80')
+          }
+          var skills70 = document.querySelectorAll(".radial-progress-70")
+          for( var c = 0; c<skills70.length; c++){
+            skills70[c].classList.add('meter-70')
+          }
+          var skills60 = document.querySelectorAll(".radial-progress-60")
+          for( var d = 0; d<skills60.length; d++){
+            skills60[d].classList.add('meter-60')
+          }
+          var skills50 = document.querySelectorAll(".radial-progress-50")
+          for( var e = 0; e<skills50.length; e++){
+            skills50[e].classList.add('meter-50')
+          }
+          var skills40 = document.querySelectorAll(".radial-progress-40")
+          for( var f = 0; f<skills40.length; f++){
+            skills40[f].classList.add('meter-40')
+          }
+          var skills30 = document.querySelectorAll(".radial-progress-30")
+          for( var g = 0; g<skills30.length; g++){
+            skills30[g].classList.add('meter-30')
+          }
+          var skills20 = document.querySelectorAll(".radial-progress-20")
+          for( var h = 0; h<skills20.length; h++){
+            skills20[h].classList.add('meter-20')
+          }
+          var skills10 = document.querySelectorAll(".radial-progress-10")
+          for( var i = 0; i<skills10.length; i++){
+            skills10[i].classList.add('meter-10')
+          }
+        }}
+      />
+      <Languages />
+      <Framework />
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        onViewportEnter={(entry) => {
+          var skills90 = document.querySelectorAll('.radial-progress-90-1')
+          for( var a = 0; a<skills90.length; a++){
+            skills90[a].classList.add('meter-90')
+          }
+          var skills80 = document.querySelectorAll(".radial-progress-80-1")
+          for( var b = 0; b<skills80.length; b++){
+            skills80[b].classList.add('meter-80')
+          }
+          var skills70 = document.querySelectorAll(".radial-progress-70-1")
+          for( var c = 0; c<skills70.length; c++){
+            skills70[c].classList.add('meter-70')
+          }
+          var skills60 = document.querySelectorAll(".radial-progress-60-1")
+          for( var d = 0; d<skills60.length; d++){
+            skills60[d].classList.add('meter-60')
+          }
+          var skills50 = document.querySelectorAll(".radial-progress-50-1")
+          for( var e = 0; e<skills50.length; e++){
+            skills50[e].classList.add('meter-50')
+          }
+          var skills40 = document.querySelectorAll(".radial-progress-40-1")
+          for( var f = 0; f<skills40.length; f++){
+            skills40[f].classList.add('meter-40')
+          }
+          var skills30 = document.querySelectorAll(".radial-progress-30-1")
+          for( var g = 0; g<skills30.length; g++){
+            skills30[g].classList.add('meter-30')
+          }
+          var skills20 = document.querySelectorAll(".radial-progress-20-1")
+          for( var h = 0; h<skills20.length; h++){
+            skills20[h].classList.add('meter-20')
+          }
+          var skills10 = document.querySelectorAll(".radial-progress-10-1")
+          for( var i = 0; i<skills10.length; i++){
+            skills10[i].classList.add('meter-10')
+          }
+        }}
+      />
+      <Database />
+      <Platform />
+    </section>
+  );
+};
+
+const Languages = () => {
+  return (
+    <div className="xl:px-5">
       <h3 className="text-2xl p-3">Language</h3>
-      <ul className="flex gap-5 xl:gap-16 overflow-x-auto ">
-        <li className=" ">
-          <div className="radial-progress bg-white" style={{ "--value": 80 }}>
-            HTML
-          </div>
+      <ul className="flex gap-5 xl:gap-16 overflow-x-auto xl:p-5">
+        <li className="">
+          <div className="radial-progress-80">HTML</div>
         </li>
         <li className="">
-          <div className="radial-progress bg-white" style={{ "--value": 70 }}>
-            CSS/SCSS
-          </div>
+          <div className="radial-progress-70">CSS/SCSS</div>
         </li>
         <li className="">
-          <div className="radial-progress bg-white" style={{ "--value": 60 }}>
-            JS
-          </div>
+          <div className="radial-progress-50">JS</div>
         </li>
         <li className="">
-          <div className="radial-progress bg-white" style={{ "--value": 30 }}>
-            JAVA
-          </div>
+          <div className="radial-progress-20">JAVA</div>
         </li>
         <li className="">
-          <div className="radial-progress bg-white" style={{ "--value": 15 }}>
-            c/c++
-          </div>
+          <div className="radial-progress-10">c/c++</div>
         </li>
       </ul>
+    </div>
+  );
+};
+
+const Framework = () => {
+  return (
+    <div className="xl:px-5">
       <h3 className="text-2xl p-3">Framework</h3>
-      <ul className="flex gap-5 xl:gap-16 overflow-x-auto">
+      <ul className="flex gap-5 xl:gap-16 overflow-x-auto xl:p-5">
         <li className="">
-          <div className="radial-progress bg-white" style={{ "--value": 40 }}>
-            React
-          </div>
+          <div className="radial-progress-40">React</div>
         </li>
         <li className="">
-          <div className="radial-progress bg-white" style={{ "--value": 10 }}>
-            Next.js
-          </div>
+          <div className="radial-progress-10">Next.js</div>
         </li>
       </ul>
+    </div>
+  );
+};
+
+const Database = () => {
+  return (
+    <div className="xl:px-5">
       <h3 className="text-2xl p-3">Database</h3>
-      <ul className="flex gap-5 xl:gap-16 overflow-x-auto">
+      <ul className="flex gap-5 xl:gap-16 overflow-x-auto xl:p-5">
         <li className="">
-          <div
-            className="radial-progress bg-white text-center"
-            style={{ "--value": 60 }}
-          >
-            MySQL
-          </div>
+          <div className="radial-progress-60-1 text-center">MySQL</div>
         </li>
         <li className="">
           <div
-            className="radial-progress bg-white text-sm"
-            style={{ "--value": 70 }}
-          >
+            className="radial-progress-70-1 text-sm">
             MongoDB
           </div>
         </li>
         <li className="">
           <div
-            className="radial-progress bg-white text-sm"
-            style={{ "--value": 40 }}
-          >
+            className="radial-progress-30-1 text-sm">
             PostgreSQL
           </div>
         </li>
       </ul>
-      <h3 className="text-2xl p-3">Plathome</h3>
-      <ul className="flex gap-5 xl:gap-16 overflow-x-auto">
+    </div>
+  );
+};
+
+const Platform = () => {
+  return (
+    <div className="xl:px-5">
+      <h3 className="text-2xl p-3">Platform</h3>
+      <ul className="flex gap-5 xl:gap-16 overflow-x-auto xl:p-5">
         <li className="">
           <div
-            className="radial-progress bg-white text-center"
-            style={{ "--value": 90 }}
+            className="radial-progress-90-1 text-center"
           >
             Visual Studio
           </div>
         </li>
         <li className="">
-          <div className="radial-progress bg-white" style={{ "--value": 80 }}>
+          <div className="radial-progress-80-1">
             Figma
           </div>
         </li>
         <li className="">
-          <div className="radial-progress bg-white" style={{ "--value": 70 }}>
-            Adbe XD
+          <div className="radial-progress-70-1">
+            Adobe XD
           </div>
         </li>
       </ul>
-
-      <div class="dashboard">
-        <svg>
-          <circle class="bg" cx="57" cy="57" r="52" />
-          <circle class="meter-1" cx="57" cy="57" r="52" />
-        </svg>
-        <svg>
-          <circle class="bg" cx="57" cy="57" r="52" />
-          <circle class="meter-2" cx="57" cy="57" r="52" />
-        </svg>
-        <svg>
-          <circle class="bg" cx="57" cy="57" r="52" />
-          <circle class="meter-3" cx="57" cy="57" r="52" />
-        </svg>
-      </div>
-    </section>
+    </div>
   );
 };
+
 export default Skills;
