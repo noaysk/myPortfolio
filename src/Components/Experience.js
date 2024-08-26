e59a8a604f662b419fddd6167d6c9cd5b8091bb6
noaysk <nyasukagawa@myseneca.ca>
2024-08-26 21:44:00 +0900

    fixed experiences

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,4 +1,3 @@
-import config from "../config";
 import { motion } from "framer-motion";
 
 const Experience = () => {
@@ -16,6 +15,9 @@ const Experience = () => {
           <li data-content="✓" className="step step-neutral">
             F Co., Ltd. - Tokyo
           </li>
+          <li data-content="✓" className="step step-neutral">
+            STUCKON - Toronto
+          </li>
           <li data-content="" className="step step-neutral">
             A graduate of Seneca College - Toronto
           </li>
@@ -25,18 +27,36 @@ const Experience = () => {
           whileInView="visible"
           viewport={{ once: true }}
           onViewportEnter={(entry) => {
-            document.querySelector('.opacityAnimation').classList.add('opacityAnimationStart') 
+            document
+              .querySelector(".opacityAnimation")
+              .classList.add("opacityAnimationStart");
           }}
         />
-        <div className="opacityAnimation bg-frame1 bg-no-repeat bg-contain w-[350px] bg-center px-3 py-12 xl:w-[500px] xl:my-0 xl:mx-auto text-cyan-950">
-          <div className="w-[256px] my-o mx-auto xl:pt-[100px] xl:pl-[65px] xl:w-[450px]">
-            <h3 className="text-2xl pb-0">F Co., Ltd. (Programmer) </h3>
-            <h4 className="text-xl pb-3">November 2021 to Jun 2023</h4>
-            <p>
-            F CO. is an consultant company who implements applications for various clients.
-            I have participated in multiple projects remotely from Canada, sometimes on site at
-            Japan during summer vacation.
-            </p>
+        <div className="bg-bgColor rounded-lg p-2  text-cyan-950">
+          <div className="w-[256px] my-o mx-auto p-5 xl:w-[450px]">
+            <div>
+              <h3 className="text-2xl pb-0">
+                F Co., Ltd. (Full-Stuck Developer){" "}
+              </h3>
+              <h4 className="text-xl pb-3">July 2021 to May 2023</h4>
+              <p>
+                F CO. is an consultant company who implements applications for
+                various clients. I have participated in multiple projects
+                remotely from Canada, sometimes on site at Japan during summer
+                vacation.
+              </p>
+            </div>
+            <div className="pt-5">
+              <h3 className="text-2xl pb-0">
+                STUCKON (Jr.Front-End Developer){" "}
+              </h3>
+              <h4 className="text-xl pb-3">Nov 2021 to Jan 2022</h4>
+              <p>
+                Participated in the development of an Android app for
+                international students in Toronto. Engaged in a study focused on
+                website development
+              </p>
+            </div>
           </div>
         </div>
       </div>
